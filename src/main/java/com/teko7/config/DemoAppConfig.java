ed97b0870fdfbe251027c7d98baea0e12caa610b
4teko7 <bilal.tekin@boun.edu.tr>
2020-05-18 16:22:50 +0300

    AbstractAnnotationConfigDispatcherServletInitializerWasAdded

---
+++ src/main/java/com/teko7/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.teko7.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.teko7")
+public class DemoAppConfig {
+
+}
