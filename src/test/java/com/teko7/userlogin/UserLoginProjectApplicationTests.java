c2250bf5105822b9726cc0538bcca75059c2c53b
4teko7 <bilal.tekin@boun.edu.tr>
2020-05-18 16:17:04 +0300

    firstCommitOfDemoSpringBootApplication

---
+++ src/test/java/com/teko7/userlogin/UserLoginProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teko7.userlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
